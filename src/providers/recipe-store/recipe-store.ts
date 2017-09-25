373aaf12f0c9bb9dffe5a063619ac622be23a683
Dominik <challu.app@gmail.com>
2017-09-25 22:23:52 +0300

    added recipe store

---
+++ src/providers/recipe-store/recipe-store.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {Recipe} from "../../models/recipe";
+import {Observable} from "rxjs/Observable";
+import {SelectRightProviderProvider} from "../select-right-provider/select-right-provider";
+import {DatabaseProvider} from "../database/database";
+import {DummyDatabaseProvider} from "../dummy-database/dummy-database";
+
+/*
+  Generated class for the RecipeStoreProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RecipeStore {
+private _recipes:BehaviorSubject<Recipe[]> = new BehaviorSubject([]);
+public readonly  recipes:Observable<Recipe[]> = this._recipes.asObservable();
+  private db: DummyDatabaseProvider | DatabaseProvider;
+
+
+
+  constructor(select: SelectRightProviderProvider) {
+    this.db = select.GetDatabaseProvider();
+
+    this.db.getDatabaseState().subscribe(rdy => {
+      if (rdy) {
+        this.db.getAllRecipes().then(value => {
+          this._recipes.next(value);
+          console.log("recipe store :" + value);
+        })
+      }
+    })
+  }
+
+
+  addRecipe(recipe:Recipe){
+    this.db.addRecipe( recipe);
+    let array = this._recipes.getValue();
+    array.push(recipe);
+    this._recipes.next(array);
+  }
+
+
+
+
+}
